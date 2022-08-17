63d59b82f1db34850cc4e635e6f20afaca01cf1b
surbh <surbh@surbhi>
2022-08-17 15:10:04 +0530

    commit jenkin

--- DemoProject1/src/test/java/com/crm/Sdet/Demo/Demo1.java
+++ DemoProject1/src/test/java/com/crm/Sdet/Demo/Demo1.java
@@ -16,22 +16,26 @@ public class Demo1 {public class DemoProject0 {
 		WebDriverManager.chromedriver().setup();
       WebDriver driver=new ChromeDriver();
       
-		FileInputStream fis=new FileInputStream("C:\\Users\\surbh\\git\\vtigerRepository\\SDET39_SeleniumFramework\\src\\test\\resources\\data.properties");
-    	Properties p=new Properties();
-    	p.load(fis);
+	//	FileInputStream fis=new FileInputStream("C:\\Users\\surbh\\git\\vtigerRepository\\SDET39_SeleniumFramework\\src\\test\\resources\\data.properties");
+    	//Properties p=new Properties();
+    	//p.load(fis);
     	
     	//String value = p.getProperty("key");
-		String Browser = p.getProperty("browser");
-		System.out.println(Browser);
+		//String Browser = p.getProperty("browser");
+		//System.out.println(Browser);
 		
-		String Url = p.getProperty("url");
-		String Username = p.getProperty("un");
-		String password = p.getProperty("pwd");
+		//String Url = p.getProperty("url");
+		//String Username = p.getProperty("un");
+		//String password = p.getProperty("pwd");
 		
-		driver.get(Url);			
-	
+		//driver.get(Url);	
+      
+	   String URL = System.getProperty("url");
+	   System.out.println("URL");
+	   driver.get(URL);
 		System.out.println("hello");
 		System.out.println("hellow world");
+		System.out.println("good morning");
 		
   }
 
